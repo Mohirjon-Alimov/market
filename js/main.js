0080db4f7dd7b7bac606a3c4b800036bbcfc855b
Mohirjon <mohir7209@gmail.com>
2022-04-19 21:28:51 +0500

    git init

---
+++ js/main.js
@@ -0,0 +1,17 @@
+
+var elBtn = document.querySelector(".btn");
+var elOut = document.querySelector(".out");
+var elInput = document.querySelector("#adding");
+var newList = document.createElement("ul");
+var things = [];
+
+elBtn.addEventListener("click", function () {
+
+  var listItem = document.createElement("li");
+  listItem.textContent = elInput.value;
+  
+  elOut.appendChild(newList);
+  newList.appendChild(listItem);
+  elOut.style.border = "1px solid aqua";
+  elOut.style.padding = "20px";
+})
