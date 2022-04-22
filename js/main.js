edb87224f630046474665a67c9ff52ba0cdfdda0
Mohirjon <mohir7209@gmail.com>
2022-04-22 16:50:52 +0500

    add checkbox

--- js/main.js
+++ js/main.js
@@ -3,7 +3,9 @@ var elBtn = document.querySelector(".btn");
 var elOut = document.querySelector(".out");
 var elInput = document.querySelector("#adding");
 var newList = document.createElement("ul");
+var elCheck = document.querySelector("#checking")
 var things = [];
+
 
 
 
@@ -13,10 +15,21 @@ elBtn.addEventListener("click", function () {
   listItem.textContent = elInput.value;
   
   var inpValue = elInput.value.trim();
-  things.push(inpValue);
+
+  var checked = elCheck.checked;
+
+  if(checked) {
+    things.unshift(inpValue);
+  }else if(!checked) {
+    things.push(inpValue);
+  }
+
+
   
   elOut.appendChild(newList);
   newList.appendChild(listItem);
   elOut.style.border = "1px solid aqua";
   elOut.style.padding = "20px";
+
+  elInput.value = null;
 })
